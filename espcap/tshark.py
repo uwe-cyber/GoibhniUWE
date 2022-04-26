b0363ea2aee63d6926a57307ea01263be1ffb508
amills157 <alanmills88@gmail.com>
2022-04-26 21:41:43 +0100

    Initial commit

---
+++ espcap/tshark.py
@@ -0,0 +1,134 @@
+""" tshark.py
+
+Wrapper API class for tshark
+
+Copyright (c) 2019 Vic Hargrave
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+"""
+
+
+import os
+import sys
+import yaml
+import json
+import signal
+import binascii
+import subprocess
+
+closing = False
+
+def _exit_gracefully(signum, frame):
+    """ Sets global closing flag.
+
+    :param signum: Signal type
+    :param frame:
+    """
+    global closing
+    closing = True
+
+
+class Tshark(object):
+
+    def __init__(self):
+        """ Sets the path to tshark.  Try all the possible paths in _config_paths"""
+        self._command = ["/usr/local/bin/tshark"]
+
+    def capture(self, command):
+        """ Generator to do packet capture.
+
+        :param command: tshark command to execute
+        :return: Packet JSON object that can be indexed in Elasticsearch
+        """
+        global closing
+        with subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=0) as proc:
+            for packet in proc.stdout:
+                packet = self._drop_index_line(packet)
+                if packet is None:
+                    continue
+                else:
+                    try:
+                        json_packet = json.loads(packet)
+                        hexvalue = json_packet["layers"]["tcp"]["tcp_tcp_payload"].replace(":","")
+                        plaintext = bytearray.fromhex(hexvalue).decode()
+                        json_packet["layers"]["tcp"]["tcp_tcp_payload"] = plaintext
+                        yield json_packet
+                    except KeyError:
+                        yield json.loads(packet)
+
+                    
+
+            if closing is True:
+                print('Capture interrupted')
+                sys.exit()
+
+    def list_interfaces(self, command):
+        """ Get all the network interfaces available.
+
+        :return: List of network interfaces
+        """
+        with subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=0) as proc:
+            for interface in proc.stdout:
+                print(interface.decode().rstrip('\n'))
+
+    def make_command(self, nic, count, bpf, pcap_file, interfaces):
+        """ Builds a tshark command to execute.
+
+        :param nic: Network interface
+        :param count: Number of packets to capture, 0 if capturing indefinitely
+        :param bpf: Packet filter expression
+        :param pcap_file: PCAP file where packets are coming from, None if live capture
+        :param interfaces: -D if just getting a list of network interfaces
+        :return: tshark with arguments array
+        """
+        command = self._command
+
+        if interfaces is True:
+            command.append('-D')
+            return command
+
+        command.append('-T')
+        command.append('ek')
+        if nic is not None:
+            command.append('-i')
+            command.append(nic)
+        if count != 0:
+            command.append('-c')
+            command.append(str(count))
+        if bpf is not None:
+            elements = bpf.split()
+            for element in elements:
+                command.append(element)
+        if pcap_file is not None:
+            command.append('-r')
+            command.append(pcap_file)
+
+        return command
+
+    # What to do when the application is interrupted.
+    def set_interrupt_handler(self):
+        """ Set interrupt handlers """
+        signal.signal(signal.SIGTERM, _exit_gracefully)
+        signal.signal(signal.SIGINT, _exit_gracefully)
+
+    def _drop_index_line(self, line):
+        """ Drops the bulk index line from the tshark packet output
+
+        :param line: Line with extra characters
+        :return: line or None if the line contains Elasticsearxh
+        """
+        decoded_line = line.decode().rstrip('\n')
+        if decoded_line.startswith('{\"index\":') is True:
+            return None
+        else:
+            return decoded_line
---
+++ goibhniu.py
@@ -0,0 +1,84 @@
+import os
+import re
+import docker
+import subprocess
+import espcap.espcap as espcap
+
+from io import BytesIO
+
+
+docker_client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+
+api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
+
+def traffic_monitor_setup(api_client):
+
+    image_list = ["elasticsearch:7.17.2","kibana:7.17.2"]
+
+    ipv4_addresses = ["172.18.0.10","172.18.0.11"]
+
+    env_args = ["discovery.type=single-node",""]
+
+    for i in range(0, len(image_list)):
+        image = image_list[i]
+        container_name = image.split(":")[0]
+        ip_addr = ipv4_addresses[i]
+
+        container = docker_client.containers.create(image, detach=True, name=container_name, environment=[env_args[i]])
+        docker_client.networks.get("uwe_tek").connect(container, ipv4_address=ip_addr)
+        container.start()
+
+# Add in clean container shutdown on keyboard interrupt
+def traffic_creation_setup():
+
+    container_names = ["blue_team", "red_team"]
+
+    ipv4_addresses = ["172.18.0.20", "172.18.0.21"]
+
+    for i in range(0, len(container_names)):
+        subprocess.Popen(['gnome-terminal', '-x', 'bash', '-c', 'docker run -it --rm --net uwe_tek --ip {} uwe_battleships:{}'.format(ipv4_addresses[i],container_names[i])], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+
+def network_create(api_client,network_subnets):
+
+    count = 18
+    
+    while True:
+        uwe_tek_subnet = "172.{}.0.0/24".format(str(count))
+
+        if uwe_tek_subnet not in network_subnets:
+            break
+        else:
+            count += 1
+
+    ipam_pool = docker.types.IPAMPool(subnet=uwe_tek_subnet)
+
+    ipam_config = docker.types.IPAMConfig(pool_configs=[ipam_pool])
+
+    api_client.create_network("uwe_tek",ipam=ipam_config)
+
+    print("uwe_tek network created - Subnet = {}".format(uwe_tek_subnet))
+
+network_list = api_client.networks()
+
+network_subnets = []
+
+uwe_tek_exists = False
+
+for network in network_list:
+    if network["Name"] == "uwe_tek":
+        print("uwe_tek network found - Subnet = {}".format(network["IPAM"]["Config"][0]["Subnet"]))
+        uwe_tek_exists = True
+        break
+    if len(network["IPAM"]["Config"]) > 0:
+        for i in range(0,len(network["IPAM"]["Config"])):
+            network_subnets.append(network["IPAM"]["Config"][i]["Subnet"])
+
+if not uwe_tek_exists:
+    network_create(api_client,network_subnets)
+
+#TODO - Add check for elasticsearch / kibana containers
+#traffic_monitor_setup(api_client)
+
+traffic_creation_setup()
+
+espcap.main("172.18.0.10:9200","any","src net 172.18.0.0/24 and host not 172.18.0.10 and host not 172.18.0.11",100,0)
